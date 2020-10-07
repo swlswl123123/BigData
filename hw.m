6432228ef4204f16eaf82134a5625a1d587eea71
swlswl123123 <1835654525@qq.com>
2020-10-07 18:15:17 +0800

    [SIM]:revise version

--- hw.m
+++ hw.m
@@ -6,18 +6,21 @@ arg_age = data(:,6);
 
 f1 = figure(1);
 histfit(arg_age);
-[mu,sigma] = normfit(arg_age)
-set(get(gca, 'XLabel'), 'String', 'age');
-set(get(gca, 'YLabel'), 'String', 'N');
-print(f1,'-dpng','-r300','./image/pdf_arg_age_TX_B.png');
+[mu,sigma] = normfit(arg_age);
+set(get(gca, 'XLabel'), 'String', 'age', 'fontsize', 16);
+set(get(gca, 'YLabel'), 'String', 'N', 'fontsize', 16);
+set(gca, 'FontSize', 16);
+print(f1,'-dpng','-r300','./image/pdf_arg_age.png');
 
 f2 = figure(2);
-qqplot(arg_age);
-print(f2,'-dpng','-r300','./image/qq_arg_age_TX_B.png');
+pd = makedist('Normal','mu',mu,'sigma',sigma);
+qqplot(arg_age, pd);
+set(get(gca, 'XLabel'), 'String', '拟合正态分位数');
+set(gca, 'FontSize', 16);
+print(f2,'-dpng','-r300','./image/qq_arg_age.png');
 
 % ks-test
-pd = makedist('Normal','mu',mu,'sigma',sigma);
-[h,p,~,c] = kstest(arg_age,'CDF',pd,'Alpha',0.01)
+[h,p,~,c] = kstest(arg_age,'CDF',pd,'Alpha',0.1);
 
 % group analysis
 
@@ -33,52 +36,78 @@ for i = 1:5
     subplot(2,5,i)
     histfit(g);
     [mu,sigma] = normfit(g);
+    set(get(gca, 'Title'), 'String', ['category:', num2str(i)]);
+    set(gca, 'FontSize', 20);
     hold on;
     subplot(2,5,i+5)
-    qqplot(g);
     pd = makedist('Normal','mu',mu,'sigma',sigma);
-    [h,p,~,c] = kstest(g,'CDF',pd,'Alpha',0.1)
+    qqplot(g, pd);
+    set(get(gca, 'XLabel'), 'String', '拟合正态分位数');
+    set(gca, 'FontSize', 20);
+    [h,p,~,c] = kstest(g,'CDF',pd,'Alpha',0.05);
 end 
 
-print(f3,'-dpng','-r300','./image/sub_arg_age_TX_B.png');
+print(f3,'-dpng','-r300','./image/sub_arg_age.png');
 
 variance
 
 % ANOVA
 category = data(:,1);
-anova1(arg_age,category)
+[p,tbl] = anova1(arg_age,category,'off');
+F_total = tbl{2,5}
 
+%other factor
 SIZE = get(0,'ScreenSize');	% 获取显示屏的像素尺寸
-f4 = figure(6);				% 创建图形窗口
-set(f4, 'position', SIZE);	% 设置图形窗口位置尺寸为屏幕大小
-sel_set = {2,11,5};
-sel_name = {'age\_dif','area','phone\_num'};
+f6 = figure(6);				% 创建图形窗口
+set(f6, 'position', SIZE);	% 设置图形窗口位置尺寸为屏幕大小
+sel_set = {2,5,7};
+sel_name = {'消息数','性别比','年龄差'};
 
 for i = 1:3
 
-    factor = log10(data(:,sel_set{i})+1);
+
+    factor = data(:,sel_set{i});
+    factor(factor==0) = 0.02;
+    factor = log(factor);
+
+    factor_var = zeros(5,1);
+    for k = 1:5
+        factor_sub = factor(data(:,1)==k);
+        factor_sub_mean = sum( factor_sub)/size(factor_sub,1);
+        factor_var(k) = sqrt(sum((factor_sub-factor_sub_mean).^2)/(size(factor_sub,1)-1));
+    end
+
+    max_factor_var = max(factor_var)
+    min_factor_var = min(factor_var)
 
     subplot(2,3,i)
     histfit(factor);
-    [mu,sigma] = normfit(factor)
+    [mu,sigma] = normfit(factor);
     set(get(gca, 'XLabel'), 'String', sel_name{i});
     set(get(gca, 'YLabel'), 'String', 'N');
+    set(gca, 'FontSize', 20);
     subplot(2,3,i+3)
-    qqplot(factor);
-
-    % ks-test
     pd = makedist('Normal','mu',mu,'sigma',sigma);
-    [h,p,~,c] = kstest(factor,'CDF',pd,'Alpha',0.01)
+    qqplot(factor, pd);
+    set(get(gca, 'XLabel'), 'String', '拟合正态分位数');
+    set(gca, 'FontSize', 20);
+    % ks-test
+    [h,p,~,c] = kstest(factor,'CDF',pd,'Alpha',0.05);
 
 end
+print(f6,'-dpng','-r300','./image/factor_analysis.png');
 
-print(f4,'-dpng','-r300','./image/factor_arg_age_TX_B.png');
+for i = 1:3
+    factor = data(:,sel_set{i});
+    % ANOVA
+    anova1(factor, category);
+end
 
-% sampling
+%simple  sampling
 
 F = zeros(10,1);
 
-for i = 1:10
+for i = 1:100
 
     R = rand(size(arg_age));
     [~,R_index] = sort(R);
@@ -91,11 +120,12 @@ for i = 1:10
     F(i)=tbl{2,5};
 end
 
-F
+F_mean = sum(F)/size(F,1)
+F_var = sum((F - F_mean).^2) / (size(F,1))
 
-% another sampling
+% systematic sampling
 
-for i = 1:10
+for i = 1:100
 
     R = rand(size(arg_age));
     [~,R_index] = sort(R);
@@ -110,4 +140,33 @@ for i = 1:10
     F(i)=tbl{2,5};
 end
 
-F
+F_mean = sum(F)/size(F,1)
+F_var = sum((F - F_mean).^2) / (size(F,1))
+
+%stratified random sampling
+
+for j = 1:100
+    D_set = cell(5,1);
+    before = [];
+    after = [];
+    for i = 1:5
+        tmp = data(data(:,1) == i,:);
+        before(i) = sum(tmp(:,6))/size(tmp,1);
+        R = rand(size(tmp));
+        [~,R_index] = sort(R);
+        tmp_random = tmp(R_index,:);
+        D_set{i} = tmp_random(1:round(size(tmp,1)*0.1),:);
+        after(i) = sum(D_set{i}(:,6))/round(size(tmp,1)*0.1);
+    end
+    
+    data_sample = cell2mat(D_set);
+    category_sample = data_sample(:,1);
+    arg_age_sample = data_sample(:,6);
+    [p,tbl] = anova1(arg_age_sample,category_sample,'off');
+    
+    F(j)=tbl{2,5};
+end
+
+F_mean = sum(F)/size(F,1)
+F_var = sum((F - F_mean).^2) / (size(F,1))
+    
